4ec7e432aa3d4f4f8220c7e9fe514fa143fea887
jaimeeherrera <jaimeeherrera22@gmail.com>
2019-06-06 13:47:17 -0500

    PoCos

---
+++ PoCos/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoCos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        //this is where the objects are called
+        {
+            DriverLicense Jaimee = new DriverLicense("Jaimee", "Herrera", "F", 012345, 10 );
+        }
+         class DriverLicense
+        {
+            public string First { get; set; }
+            public string Last { get; set; }
+            public string Gender { get; set; }
+            public int LicenseNum { get; set; }
+            public int Age { get; set; }
+
+            public  DriverLicense (string First, string Last, string Gender, int LicenseNum, int Age)
+                {
+                this.First = First;
+                this.Last = Last;
+                this.Gender = Gender;
+                this.LicenseNum = LicenseNum;
+                this.Age = Age;
+                }
+            public string fullName()
+            {
+                return (First + Last); 
+            }
+
+        }
+
+
+        class Book
+        {
+            public string Title { get; set; }
+            // adding to the list for multiple authors 
+            public List<string> Authors { get; set; }
+            public int Pages { get; set; }
+            public int SKU { get; set; }
+            public string Publisher { get; set; }
+            public float Price { get; set; }
+
+
+            public Book(string Title, string Authors, int Pages, int SKU, string Publish, float Price)
+
+
+        }
+        class Airplane
+        {
+            public string Manufauter { get; set; }
+            public string Model { get; set; }
+            public string Variant { get; set; }
+            public int Capacity { get; set; }
+            public int NumOfEngines { get; set; }
+
+            public Airplane ()
+            {
+
+            }
+        }
+    }
+  
---
+++ ToDoList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+        class ToDoItem
+        {
+            string question = "Would you like to add a ToDo item? yes/no";
+            Console.WriteLine (question);
+            string answer = Console.ReadLine().ToLower();
+
+            
+           
+            public ToDoItem()
+            {
+                public string 
+                //Ask Discription Due Date and Proirity
+                //Instantiate ToDo Item object 
+               //  to DoItem my Item= new(description, Due date, priority);
+                // Add myItem to your list  
+            }
+        }
+
+    }
+}
+}
